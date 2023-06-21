be3448154e658dde3095a8c5d5e5cb215db6fdf6
Sabas <s@electroniccats.com>
2023-06-20 18:41:32 -0600

    comment

--- scenes/mag_scene_about.c
+++ scenes/mag_scene_about.c
@@ -11,10 +11,11 @@ void mag_scene_about_on_enter(void* context) {
     furi_string_cat_printf(tmp_str, "Developer: %s\n", MAG_DEVELOPER);
     furi_string_cat_printf(tmp_str, "GitHub: %s\n\n", MAG_GITHUB);
 
-    furi_string_cat_printf(
+    /*furi_string_cat_printf(
         tmp_str,
         "Unfinished port of Samy Kamkar's MagSpoof. Confer GitHub for updates; in the interim, use responsibly and at your own risk.");
 
+    */
     // TODO: Add credits
 
     widget_add_text_scroll_element(widget, 0, 0, 128, 64, furi_string_get_cstr(tmp_str));
